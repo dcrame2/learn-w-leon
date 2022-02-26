673eae5b3af47d8034cde6369375e501bd25e80e
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-02-26 10:10:53 -0600

    class 14 complete

--- class14-materials/simple-functions/js/main.js
+++ class14-materials/simple-functions/js/main.js
@@ -1,5 +1,20 @@
 //create a function that adds two numbers and alerts the sum
 
+function addSum(num1, num2) {
+  alert(num1 + num2);
+}
+
+addSum(1, 2);
+
 //create a function that multiplys three numbers and console logs the product
+function multiplys(num1, num2, num3) {
+  console.log(num1 * num2 * num3);
+}
+multiplys(2, 3, 4);
 
 //create a function that divides two numbers and returns the ???
+function divides(num1, num2) {
+  return num1 / num2;
+}
+
+console.log(divides(3, 2));
--- class14-materials/temperature-converter/js/main.js
+++ class14-materials/temperature-converter/js/main.js
@@ -6,5 +6,5 @@ document.querySelector("#yell").addEventListener("click", run);
 function run() {
   let temp = Number(document.querySelector("#temp").value);
   temp = (temp * 9) / 5 + 32;
-  console.log(temp);
+  document.querySelector("#placeToYell").innerText = temp;
 }
