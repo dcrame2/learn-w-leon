af3924a9887460c4c097d231d2ab0e85d7361c69
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-02-25 21:39:39 -0600

    class 14 finishes

--- class14-materials/temperature-converter/js/main.js
+++ class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,10 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+// Get value from Input
+document.querySelector("#yell").addEventListener("click", run);
+
+function run() {
+  let temp = Number(document.querySelector("#temp").value);
+  temp = (temp * 9) / 5 + 32;
+  console.log(temp);
+}
