0b1447d64831128166f7ab50cbd7cd080daf6e6c
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-03-03 19:47:43 -0600

    class 16 almost done

--- class16-materials/age-checker/js/main.js
+++ class16-materials/age-checker/js/main.js
@@ -6,8 +6,30 @@
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
 
-
 //--- Harder
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+
+const h1 = document.querySelector("h1");
+
+const p = document.querySelector("p");
+h1.addEventListener("click", function () {
+  const age = Number(document.querySelector("#danceDanceRevolution").value);
+  if (age < 16) {
+    p.innerText = "they can not drive";
+  } else if (age < 18) {
+    p.innerText =
+      "they can't hate from outside the club, because they can't even get in";
+  } else if (age < 21) {
+    p.innerText = "they can not drink";
+  } else if (age < 25) {
+    p.innerText = "they can not rent cars affordably ";
+  } else if (age < 30) {
+    p.innerText = "they can not rent fancy cars affordably";
+  } else {
+    p.innerText = "there is nothing left to look forward too";
+  }
+
+  console.log(age);
+});
--- class16-materials/variable-fun-time/js/main.js
+++ class16-materials/variable-fun-time/js/main.js
@@ -1,18 +1,24 @@
 //--- Easy
 //create a variable and assign it a number
-
+let x = 20;
 //minus 10 from that number
-
+x = x - 10;
 //print that number to the console
-
+console.log(x);
 //--- Medium
 //create a variable that holds a value from the input
 
-//add 25 to that number
-
-//alert that number
-
+// //add 25 to that number
+// input = input + 25;
+// //alert that number
+// alert(input);
 //--- Hard
 //create a variable that holds the h1
-
+const h1 = document.querySelector("h1");
 //add an event listener to that element that console logs the sum of the two previous variables
+h1.addEventListener("click", sum);
+
+function sum() {
+  let input = document.querySelector("#danceDanceRevolution").value;
+  console.log(x + Number(input));
+}
