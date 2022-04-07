1e1b12422e170025891bfe127a499b78a7ac8c35
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-04-07 07:30:42 -0500

    objects

--- class23-materials/objects-lost-galaxy/js/main.js
+++ class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,19 @@
 //Create a dog object that has four properties and three methods
+const dog = {};
+
+dog.breed = "poodle";
+dog.color = "golden";
+dog.legs = 4;
+dog.name = "Fido";
+
+dog.bark = function () {
+  console.log("WOOOOF");
+};
+
+dog.jump = function () {
+  console.log("BLAST OFF");
+};
+
+dog.fetch = function () {
+  console.log("ZOOOOOOMMMMM");
+};
--- class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,25 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+
+function ProSkater(name, height, age, move) {
+  (this.name = name),
+    (this.height = height),
+    (this.age = age),
+    (this.specialMove = move),
+    (this.trick = function () {
+      console.log("I just did a back flip");
+    });
+
+  this.flip = function () {
+    console.log("Kickflip!!");
+  };
+
+  this.taunt = function () {
+    console.log(`Don't make me use my ${this.specialMove}`);
+  };
+
+  this.grab = function () {
+    console.log("MELON GRABBB");
+  };
+}
+
+let kareemCampbell = new ProSkater("Kareem", 69, 25, "Ghetto Bird");
