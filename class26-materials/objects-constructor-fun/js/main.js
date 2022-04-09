0285fde53013a5282fbb5772e36564cbb9a105b4
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-04-09 09:27:10 -0500

    contructor practice

--- class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,26 @@
 //Create a constructor with 4 properties and 3 methods
+function PizzaMachine(size, toppings, crust, sauce) {
+  this.pizzaSize = size;
+  this.pizzaToppings = toppings;
+  this.pizzaCrust = crust;
+  this.pizzaSauce = sauce;
+
+  this.estimatedDeliveryTime = function () {
+    console.log("Calculating....");
+  };
+
+  this.burnMouth = function () {
+    console.log("FAFoijwognvronr");
+  };
+
+  this.frisbee = function () {
+    console.log("YEEEeeeeet");
+  };
+}
+
+let pizza = new PizzaMachine(
+  "large",
+  ["spinach", "garlic"],
+  "deep dish",
+  "white"
+);
--- class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,19 @@
 //Create a dog object that has four properties and three methods
+let pizza = {};
+
+pizza.size = "large";
+pizza.toppings = ["spinach", "onions", "jalapenos", "banana peppers", "garlic"];
+pizza.crust = "stuffed";
+pizza.sauce = "heavy";
+
+pizza.estimatedDeliveryTime = function () {
+  console.log("Calculating....");
+};
+
+pizza.burnMouth = function () {
+  console.log("FAFoijwognvronr");
+};
+
+pizza.frisbee = function () {
+  console.log("YEEEeeeeet");
+};
