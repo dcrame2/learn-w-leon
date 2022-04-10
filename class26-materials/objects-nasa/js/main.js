dc9f92bbe14ac2ad00404f8ab1931c4a8ba1ed29
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-04-10 09:46:31 -0500

    good updates to API

--- class26-materials/objects-nasa/js/main.js
+++ class26-materials/objects-nasa/js/main.js
@@ -1,18 +1,19 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
 
-const apiKey = "u6kWczvgiyX3G4A4CL7rBul3nkTf626xfidaoS7J";
-const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
-
 document.querySelector("button").addEventListener("click", getDate);
 
 function getDate() {
+  let newDate = document.querySelector("input").value.toString();
+  const apiKey = "u6kWczvgiyX3G4A4CL7rBul3nkTf626xfidaoS7J";
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${newDate}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      console.log(document.querySelector("input").value);
+      console.log(document.querySelector("input").value.toString());
+      console.log(url);
 
-      data.date = document.querySelector("input").value.toString();
+      //   data.date = newDate;
 
       document.querySelector("h2").innerText = data.title;
       document.querySelector("img").src = data.hdurl;
