af2a32d71e65b1697eda81de7698bde23627f33a
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-04-14 19:34:26 -0500

    playing around with APIs

--- class27-materials/api-template/js/main.js
+++ class27-materials/api-template/js/main.js
@@ -1,17 +1,31 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+function getFetch() {
+  const choice = document.querySelector("input").value;
+
+  console.log(choice);
+  const url = `https://api.nasa.gov/planetary/apod?api_key=u6kWczvgiyX3G4A4CL7rBul3nkTf626xfidaoS7J&date=${choice}`;
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+
+      if (data.media_type === "image") {
+        document.querySelector("img").src = data.hdurl;
+      } else if (data.media_type === "video") {
+        document.querySelector("iframe").src = data.url;
+      }
+
+      document.querySelector("h3").innerText = data.explanation;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
 
+// const apiKey = "u6kWczvgiyX3G4A4CL7rBul3nkTf626xfidaoS7J";
+// const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${newDate}`;
+
+// https://api.nasa.gov/planetary/apod?api_key=u6kWczvgiyX3G4A4CL7rBul3nkTf626xfidaoS7J
