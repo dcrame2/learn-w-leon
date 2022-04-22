3847a0c8aec64583ada987fb9ea2c22324e9c97a
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-04-22 18:35:44 -0500

    dnd api

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -1,17 +1,19 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+function getFetch() {
+  const choice = document.querySelector("input").value;
+  const url = `https://www.dnd5eapi.co/api/spells/${choice}`;
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-       
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
 
+      document.querySelector("h2").innerText = data.name;
+      document.querySelector("h3").innerText = data.subclasses[0].name;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
