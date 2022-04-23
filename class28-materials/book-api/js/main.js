c1450581b17d21bd8c74344c04d90609561fb48b
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-04-23 12:43:03 -0500

    book api

--- class28-materials/book-api/js/main.js
+++ class28-materials/book-api/js/main.js
@@ -1,6 +1,8 @@
 //Example fetch using pokemonapi.co
 document.querySelector("button").addEventListener("click", getFetch);
 
+document.querySelector("h2").innerText = localStorage.getItem("books");
+
 function getFetch() {
   const choice = document.querySelector("input").value;
   console.log(choice);
@@ -10,6 +12,10 @@ function getFetch() {
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       console.log(data.title);
+      //put title into localStorage
+      let books = localStorage.getItem("books") + " ; " + data.title;
+      localStorage.setItem("books", books);
+      document.querySelector("h2").innerText = localStorage.getItem("books");
     })
     .catch((err) => {
       console.log(`error ${err}`);
