c71d6ba1ca6207ec3c2febb7a87fd59649e19f13
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-04-22 19:06:25 -0500

    dnd api

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -8,10 +8,21 @@ function getFetch() {
   fetch(url)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
-      console.log(data);
+      console.log(data.subclasses);
 
-      document.querySelector("h2").innerText = data.name;
-      document.querySelector("h3").innerText = data.subclasses[0].name;
+      //   document.querySelector("h2").innerText = data.name;
+      //   document.querySelector("h3").innerText = data.subclasses[0].name;
+
+      data.subclasses.forEach((obj) => {
+        console.log(obj.name);
+
+        //create an li
+        const li = document.createElement("li");
+        //add text to li
+        li.textContent = obj.name;
+        //append the li to the ul
+        document.querySelector("ul").appendChild(li);
+      });
     })
     .catch((err) => {
       console.log(`error ${err}`);
