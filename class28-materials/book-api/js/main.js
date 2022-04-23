e21dee7fbdd756d7c5689d8fe3a9bbdd6f8ea5f8
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-04-23 12:27:06 -0500

    book api

---
+++ class28-materials/book-api/js/main.js
@@ -0,0 +1,17 @@
+//Example fetch using pokemonapi.co
+document.querySelector("button").addEventListener("click", getFetch);
+
+function getFetch() {
+  const choice = document.querySelector("input").value;
+  console.log(choice);
+  const url = `https://openlibrary.org/isbn/${choice}.json`;
+
+  fetch(url)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data.title);
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
