dd61563481a2228cbd78b9835518ec23f02b0bde
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-04-24 12:53:42 -0500

    book api

--- class28-materials/book-api/js/main.js
+++ class28-materials/book-api/js/main.js
@@ -12,9 +12,13 @@ function getFetch() {
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       console.log(data.title);
+      if (!localStorage.getItem("books")) {
+        localStorage.setItem("books", data.title);
+      } else {
+        let books = localStorage.getItem("books") + " ; " + data.title;
+        localStorage.setItem("books", books);
+      }
       //put title into localStorage
-      let books = localStorage.getItem("books") + " ; " + data.title;
-      localStorage.setItem("books", books);
       document.querySelector("h2").innerText = localStorage.getItem("books");
     })
     .catch((err) => {
