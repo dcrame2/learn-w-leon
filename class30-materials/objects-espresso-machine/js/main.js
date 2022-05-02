5a8bba120c60015268a23ef830b4d72111248955
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-05-02 07:41:59 -0500

    objects review

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,21 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class EspressoMachine {
+  constructor(color, make, model, price) {
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.price = price;
+  }
+  turnOn() {
+    console.log("Good day, I am now on!");
+  }
+  steam() {
+    console.log("ITSSS SUMMER SALLLLLE 50%%%%");
+  }
+
+  brew() {
+    console.log("Good stuff coming your way!");
+  }
+}
+
+let gaggia = new EspressoMachine("red", "gaggia", "Classic Pro", 400);
