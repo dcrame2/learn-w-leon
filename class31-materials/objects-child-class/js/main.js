364980ec40cabe5d1d0078f272cdb30edf298194
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-05-23 07:24:15 -0500

    class 31 updates

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,27 @@
-//Create an a class and extend it - Can be anything you would like it to be! 
+//Create an a class and extend it - Can be anything you would like it to be!
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {
+    console.log(`${this.name} makes a sound`);
+  }
+}
 
+class Cheetah extends Animal {
+  constructor(name, speed) {
+    super(name);
+    this.speed = speed;
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, legs) {
+    super(name);
+    this.legs = legs;
+  }
+
+  speak() {
+    console.log("moew");
+  }
+}
