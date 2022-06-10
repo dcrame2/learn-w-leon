fd3b2f3bf1e59f444357a3e804e98d24d4078190
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-06-10 07:27:59 -0500

    update to main.js

--- class35-materials/async-practice/js/main.js
+++ class35-materials/async-practice/js/main.js
@@ -27,20 +27,20 @@
 // houseThree();
 
 //Code 03
-function houseOne() {
-  console.log("Paper delivered to house 1");
-}
-function houseTwo(callback) {
-  setTimeout(() => {
-    console.log("Paper delivered to house 2");
-    callback();
-  }, 3000);
-}
-function houseThree() {
-  console.log("Paper delivered to house 3");
-}
-houseOne();
-houseTwo(houseThree);
+// function houseOne() {
+//   console.log("Paper delivered to house 1");
+// }
+// function houseTwo(callback) {
+//   setTimeout(() => {
+//     console.log("Paper delivered to house 2");
+//     callback();
+//   }, 3000);
+// }
+// function houseThree() {
+//   console.log("Paper delivered to house 3");
+// }
+// houseOne();
+// houseTwo(houseThree);
 
 //Code 04
 // function houseOne(){
@@ -58,81 +58,79 @@ houseTwo(houseThree);
 
 //Code 05
 // const promise = new Promise((resolve, reject) => {
-//     const error = false
-//     if(!error){
-//         resolve('Promise has been fullfilled')
-//     }else{
-//         reject('Error: Operation has failed')
-//     }
-// })
-// console.log(promise)
-// promise
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
+//   const error = false;
+//   if (!error) {
+//     resolve("Promise has been fullfilled");
+//   } else {
+//     reject("Error: Operation has failed");
+//   }
+// });
+// console.log(promise);
+// promise.then((data) => console.log(data)).catch((err) => console.log(err));
 
 //Code 06
-// function houseOne(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 1')
-//         }, 1000)
-//     })
+// function houseOne() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Paper delivered to house 1");
+//     }, 1000);
+//   });
 // }
-// function houseTwo(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 2')
-//         }, 5000)
-//     })
+// function houseTwo() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Paper delivered to house 2");
+//     }, 5000);
+//   });
 // }
-// function houseThree(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 3')
-//         }, 2000)
-//     })
+// function houseThree() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Paper delivered to house 3");
+//     }, 2000);
+//   });
 // }
 // houseOne()
-//     .then(data => console.log(data))
-//     .then(houseTwo)
-//     .then(data => console.log(data))
-//     .then(houseThree)
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
+//   .then((data) => console.log(data))
+//   .then(houseTwo)
+//   .then((data) => console.log(data))
+//   .then(houseThree)
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
 
 //Code 07
-// function houseOne(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 1')
-//         }, 1000)
-//     })
-// }
-// function houseTwo(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 2')
-//         }, 5000)
-//     })
-// }
-// function houseThree(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 3')
-//         }, 2000)
-//     })
-// }
+function houseOne() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Paper delivered to house 1");
+    }, 1000);
+  });
+}
+function houseTwo() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Paper delivered to house 2");
+    }, 5000);
+  });
+}
+function houseThree() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Paper delivered to house 3");
+    }, 2000);
+  });
+}
 
-// async function getPaid(){
-//     const houseOneWait = await houseOne()
-//     const houseTwoWait = await houseTwo()
-//     const houseThreeWait = await houseThree()
-//     console.log(houseOneWait)
-//     console.log(houseTwoWait)
-//     console.log(houseThreeWait)
-// }
+async function getPaid() {
+  const houseOneWait = await houseOne();
+  const houseTwoWait = await houseTwo();
+  const houseThreeWait = await houseThree();
+  console.log(houseOneWait);
+  console.log(houseTwoWait);
+  console.log(houseThreeWait);
+}
 
-// getPaid()
+getPaid();
 
 //Code 08
 // async function getACuteDogPhoto(){
